13044cb8a72964ebe9d5d42561747300989649db
arifpatanduk <arifpatanduk2@gmail.com>
2023-10-26 07:59:06 +0700

    server creation API

---
+++ app/api/servers/route.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from "uuid";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { MemberRole } from "@prisma/client";
+
+export async function POST(req: Request) {
+  try {
+    const { name, imageUrl } = await req.json();
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    // create new servers
+    const server = await db.server.create({
+      data: {
+        profileId: profile.id,
+        name,
+        imageUrl,
+        inviteCode: uuidv4(),
+        channels: {
+          create: [{ name: "general", profileId: profile.id }], // there is always general channel
+        },
+        members: {
+          create: [
+            { profileId: profile.id, role: MemberRole.ADMIN }, // the owner of the server should be the admin
+          ],
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVERS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
