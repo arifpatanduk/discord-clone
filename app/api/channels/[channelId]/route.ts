91de9a4b8b7cc432067a461bb938f0ff52a27cec
arifpatanduk <arifpatanduk2@gmail.com>
2024-01-15 21:42:31 +0700

    edit channel functionality

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -55,3 +55,65 @@ export async function DELETE(
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+
+    // if profile not found
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    // if server id not found
+    if (!serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+
+    // if channel id not found
+    if (!params.channelId) {
+      return new NextResponse("Channel ID Missing", { status: 400 });
+    }
+
+    // if name is general
+    if (name === "general") {
+      return new NextResponse("Name cannot be 'general'", { status: 400 });
+    }
+
+    // query for updating channel
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                not: "general",
+              },
+            },
+            data: { name, type }, // data to be updated
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
