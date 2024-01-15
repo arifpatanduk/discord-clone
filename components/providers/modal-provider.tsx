009945330eafede212360039435f8c75508d38fa
arifpatanduk <arifpatanduk2@gmail.com>
2024-01-15 21:03:55 +0700

    delete channel functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,7 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 
 import { useEffect, useState } from "react";
 
@@ -28,6 +29,7 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
     </>
   );
 };
