3ba9ae36866daf31593e734f50777c2ac284a8df
arifpatanduk <arifpatanduk2@gmail.com>
2024-01-24 11:15:49 +0700

    create chat header and sidebar toggle button

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,47 @@
-const ChannelIdPage = () => {
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  // check profile
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  // fetch channel using the ID
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  // find member using serverID and profileID
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) return redirect("/");
+
   return (
-    <div>
-      <div>Channel ID Page</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
     </div>
   );
 };
