abf3c0f487a5a472a2f57b752c32de0a4279d373
arifpatanduk <arifpatanduk2@gmail.com>
2024-02-11 21:22:54 +0700

    update & delete message api

---
+++ pages/api/socket/messages/[messageId].ts
@@ -0,0 +1,149 @@
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { NextApiResponseServerIo } from "@/types";
+
+import { NextApiRequest } from "next";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method != "DELETE" && req.method != "PATCH") {
+    return res.status(404).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const profile = await currentProfilePages(req);
+    const { messageId, serverId, channelId } = req.query;
+    const { content } = req.body;
+
+    if (!profile) return res.status(401).json({ error: "Unauthorized" });
+
+    if (!serverId) return res.status(400).json({ error: "Server ID Missing" });
+
+    if (!channelId) {
+      return res.status(400).json({ error: "Channel ID Missing" });
+    }
+
+    // find the server
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId as string,
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+    if (!server) {
+      return res.status(404).json({ error: "Server not found" });
+    }
+
+    // find the channel
+    const channel = await db.channel.findFirst({
+      where: {
+        id: channelId as string,
+        serverId: serverId as string,
+      },
+    });
+    if (!channel) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+
+    // get individual member
+    const member = server.members.find(
+      (member) => member.profileId === profile.id
+    );
+    if (!member) {
+      return res.status(404).json({ error: "Member not found" });
+    }
+
+    // find message
+    let message = await db.message.findFirst({
+      where: {
+        id: messageId as string,
+        channelId: channelId as string,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+    if (!message || message.deleted) {
+      return res.status(404).json({ error: "Message not found" });
+    }
+
+    // define the access to the message
+    const isMessageOwner = message.memberId === member.id;
+    const isAdmin = member.role === MemberRole.ADMIN;
+    const isModerator = member.role === MemberRole.MODERATOR;
+    const canModify = isMessageOwner || isAdmin || isModerator;
+
+    if (!canModify) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // if the request is DELETE (deleting)
+    if (req.method === "DELETE") {
+      // soft delete the message
+      message = await db.message.update({
+        where: {
+          id: messageId as string,
+        },
+        data: {
+          fileUrl: null,
+          content: "This message has been deleted",
+          deleted: true,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+    }
+
+    // if the request is PATCH (updating)
+    if (req.method === "DELETE") {
+      // check the owner first
+      if (!isMessageOwner) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      //  update the message content
+      message = await db.message.update({
+        where: {
+          id: messageId as string,
+        },
+        data: {
+          content: content,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+    }
+
+    const updateKey = `chat:${channelId}:message:update`;
+    res?.socket?.server?.io?.emit(updateKey, message);
+
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("[MESSAGE_ID]", error);
+    return res.status(500).json({ error: "Internal Error" });
+  }
+}
